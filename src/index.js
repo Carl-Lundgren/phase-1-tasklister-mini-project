ac4b8346d7a3be8d817b009dec1eb2f0ad384a5a
Carl <lundgren.e.carl@gmail.com>
2021-04-08 20:28:56 -0700

    Complete w/ Delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector('#new-task-description');
+    if (input.value) {
+      renderTask(input.value);
+      event.target.reset();
+    }
+  });
+
+  function renderTask(task) {
+    const list = document.querySelector('#tasks'); 
+    const item = document.createElement('li');
+    const deleteBtn = document.createElement('span');
+    deleteBtn.innerText = ' ❌';
+    deleteBtn.addEventListener('click', (e) => {
+      e.target.parentNode.remove();
+    });
+    item.innerText = task;
+    item.append(deleteBtn);
+    list.append(item);
+  }
 });
